67a58d9ef02ac6b4326e355faaff5098209a87fc
Reholly <eu.ustiantsev@yandex.ru>
2024-05-08 16:41:08 +0500

    fix time

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,7 +5,6 @@ import (
 	"article-service/internal/app"
 	"article-service/internal/config"
 	"context"
-	"time"
 )
 
 const (
@@ -23,8 +22,6 @@ const (
 // @description Все тела запросов, необходимые токены и возможные ошибки указаны в описании методов.
 
 func main() {
-	time.Sleep(10 * time.Second)
-
 	cfg, err := config.LoadConfig(ConfigDevPath)
 	if err != nil {
 		panic(err)
